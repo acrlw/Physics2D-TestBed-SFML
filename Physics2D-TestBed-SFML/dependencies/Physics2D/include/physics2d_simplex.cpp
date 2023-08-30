f4b784c62bac58397255c9c6bc6208351eb8a4fb
ACRL <angryaccelerated@qq.com>
2023-08-30 16:13:08 +0800

    remove empty files

--- Physics2D-TestBed-SFML/dependencies/Physics2D/include/physics2d_simplex.cpp
+++
@@ -1,6 +0,0 @@
-#include "physics2d_simplex.h"
-
-bool Physics2D::Simplex::contains(const SimplexVertex& vertex, const real& epsilon)
-{
-    return false;
-}
--- Physics2D-TestBed-SFML/include/testbed.h
+++ Physics2D-TestBed-SFML/include/testbed.h
@@ -72,7 +72,7 @@ namespace Physics2D
 		bool m_cameraViewportMovement = false;
 
 
-		int m_currentItem = 11;
+		int m_currentItem = 0;
 
 		Frame* m_currentFrame = nullptr;
 
